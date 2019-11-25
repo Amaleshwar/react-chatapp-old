4ffa3a6ae3c5140eb511b37f83479a15c20ec977
Glitch (inchat) <none>
2019-11-25 14:17:18 +0000

    🎥⛑ Checkpoint ./src/ChatBox.js:5951290/1251 ./src/App.css:5951290/14624

--- src/ChatBox.js
+++ src/ChatBox.js
@@ -7,7 +7,31 @@ class ChatBox extends Component {
         this.state = {  }
     }
     render() { 
-        return ( <div></div> );
+        return ( 
+            <div className="chatbox-app ">
+                <div  className="chatbox-header">
+                    
+                </div>
+                <div  className="chatbox-body">
+                    <div  className="chatbox-userList">
+                            Amal <br/>Subhaga
+                    </div>
+                    <div  className="chatbox-message">
+                        hello
+                        <br/>
+                        hi
+                        <br/> hello
+                        <br/>
+                        hi
+                        <br/> hello
+                        <br/>
+                        hi
+                        <br/>
+
+                    </div>
+                </div> 
+        </div> 
+        );
     }
 }
  
